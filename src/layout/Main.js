e27f7e709491683626ed7f200c3b62378b277ce9
mitaly-dev <mitalyy.dev@gmail.com>
2022-12-03 00:33:05 -0800

    datum dashboard

---
+++ src/layout/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useContext } from 'react';
+import { Outlet } from 'react-router-dom';
+import { DashboardContext } from '../context/DashboardProvider';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import Navbar from '../Shared/Navbar';
+
+const Main = () => {
+    const {sidebarOpen,showSidebar} = useContext(DashboardContext)
+   
+    return (
+        <>
+        <div className={`min-h-screen bg-body dark:bg-lightDark flex ${showSidebar && "relative"}`}>
+            <div className={`${sidebarOpen ? 'w-[261px]' : 'w-[80px]'} duration-500 absolute inset-y-0 left-0 -translate-x-full md:relative md:translate-x-0 ${showSidebar && 'relative -translate-x-0 z-50'}`}>
+            <Dashboard></Dashboard>
+            </div>
+            <div className={`flex-1 absolute w-full md:relative`}>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Main;
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useContext } from 'react';
+import logo from '../../assets/images/logo.png'
+import { DashboardContext } from '../../context/DashboardProvider';
+import DashboardItem from './DashboardItem';
+import { menus } from './data';
+
+const Dashboard = () => {
+    const {sidebarOpen, showSidebar,setShowSidebar} = useContext(DashboardContext)
+    return (
+        <div className='p-4 pt-0 text-white bg-primary dark:bg-dark '>
+            <div className='flex items-center justify-between sticky top-0 bg-primary dark:bg-dark pt-5 pb-3 w-full'>
+                <div className='flex items-center'>
+                    <img src={logo} alt="logo" className='w-12 pr-2 '/>
+                    <h3 className={`font-semibold text-[19px] font-dmSans tracking-wider ${!sidebarOpen && 'hidden'}`}>
+                        Datum
+                    </h3>
+                </div>
+                {/* close btn for responsive */}
+                <button onClick={()=>setShowSidebar(!showSidebar)} className="md:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+           <div>
+                {
+                    menus.map((menu,index)=><DashboardItem key={index} menu={menu}></DashboardItem>)
+                }
+           </div>
+        </div>
+    );
+};
+
+export default Dashboard;
